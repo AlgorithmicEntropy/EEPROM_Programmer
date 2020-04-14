c9e51b841a215b13353e7959914384dddaa77c85
SebastianWallat <wallatsebastian@gmail.com>
2020-04-14 17:50:13 +0200

    added files

---
+++ cmake-build-debug/CMakeFiles/SysProg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SysProg.dir/main.cpp.obj"
+  "SysProg.pdb"
+  "SysProg.exe"
+  "SysProg.exe.manifest"
+  "libSysProg.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SysProg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
