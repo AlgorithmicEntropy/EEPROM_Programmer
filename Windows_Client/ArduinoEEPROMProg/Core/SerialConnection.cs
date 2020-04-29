7696f902856fa19df43d113cc4a631d8cab87e7b
Sebastian <wallatsebastian@gmail.com>
2020-04-29 16:18:23 +0200

    Added windows client c# implementation

---
+++ Windows_Client/ArduinoEEPROMProg/Core/SerialConnection.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace ArduinoEEPROMProg.Core
+{
+    public class SerialConnection : IDisposable
+    {
+        //const
+        private const byte EscapeByte = 0x0F;
+        private const byte AcknoladgeByte = 0x55;
+        private const byte ErrorByte = 0x12;
+
+        private byte lastByte = 0;
+
+        private readonly SerialPort serial;
+        private readonly Thread readThread;
+
+        public SerialConnection(string port)
+        {
+            serial = new SerialPort
+            {
+                PortName = port,
+                BaudRate = 9600,
+                Parity = Parity.None,
+                Handshake = Handshake.XOnXOff,
+                ReadTimeout = 500,
+                WriteTimeout = 500
+            };
+
+            serial.Open();
+            serial.DiscardInBuffer();
+            serial.DiscardOutBuffer();
+
+            readThread = new Thread(Read);
+            readThread.Start();
+        }
+
+        public void Dispose()
+        {
+            serial.Dispose();
+        }
+
+        public void Write(byte[] data)
+        {
+            foreach (var b in data)
+            {
+                serial.Write(new byte[] { b }, 0, 1);
+            }
+        }
+
+        private void Read()
+        {
+            while (true)
+            {
+                int count = serial.BytesToRead;
+                if (count != 0)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        var b = (byte)serial.ReadByte();
+                        switch(b)
+                        {
+                            case AcknoladgeByte:
+                                Console.WriteLine("ACK");
+                                break;
+
+                            case ErrorByte:
+                                Console.WriteLine("ERR");
+                                break;
+                            case EscapeByte:
+                                lastByte = b;
+                                break;
+
+                            default:
+                                if (lastByte == EscapeByte)
+                                {
+                                    string s = ((byte)(~b)).ToString("X2");
+                                    Console.WriteLine(s);
+                                }
+                                else
+                                {
+                                    Console.WriteLine(b.ToString("X2"));
+                                }
+                                lastByte = b;
+                                break;
+                        }
+
+                    }
+                }
+
+                Thread.Sleep(10);
+            }
+        }
+
+    }
+}
---
+++ Windows_Client/ArduinoEEPROMProg/EEPROMProgramer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+using ArduinoEEPROMProg.Core;
+using System.IO;
+
+namespace ArduinoEEPROMProg
+{
+    class EEPROMProgramer
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //get port
+            Console.WriteLine("Current serial ports:");
+
+            foreach (var p in SerialPort.GetPortNames())
+            {
+                Console.WriteLine($"\t{p}");
+            }
+
+            SerialConnection con = null;
+
+            while (con == null)
+            {
+                try
+                {
+                    string port = Console.ReadLine();
+                    con = new SerialConnection(port.ToUpper());
+                }
+                catch (System.IO.IOException)
+                {
+                    Console.WriteLine("Invalid ports");
+                } 
+            }
+
+            //clear
+            Console.Clear();
+
+            while (true)
+            {
+                string entry = Console.ReadLine();
+                if (entry == "read")
+                {
+                    byte[] code = new byte[] { 0x02, 0x00, 0x00, 0x06, 0x00, 0xFF, 0x04, 0x00, 0x00 };
+                    con.Write(code);
+                    continue;
+                }
+
+                else if (entry == "write")
+                {
+                    //get value
+                    byte val = Convert.ToByte(Console.ReadLine(), 16);
+                    byte[] code = new byte[] { 0x01, 0x00, 0x00 };
+                    //send
+                    con.Write(code);
+                    for (ushort i = 0; i <= 0x00FF; i++)
+                    {
+                        byte addH = (byte)(i >> 8);
+                        byte addL = (byte)i;
+                        Console.WriteLine($"write: {addH:X2}{addL:X2}\t{val:X2} ");
+                        con.Write(new byte[] { addH, addL, val}); 
+                    }
+                    //end
+                    con.Write(new byte[] { 0xFF, 0x00, 0x00 });
+                    continue;
+                }
+
+                try
+                {
+                    byte data = Convert.ToByte(entry, 16);
+                    con.Write(new byte[] { data });
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+
+    }
+}
