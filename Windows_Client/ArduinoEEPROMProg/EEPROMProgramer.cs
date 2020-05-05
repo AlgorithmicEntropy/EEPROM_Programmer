dc33f337a608a65df16bdfd18a38b13dd91a63b6
Sebastian <wallatsebastian@gmail.com>
2020-05-05 17:46:05 +0200

    Improved c# client functionality

--- Windows_Client/ArduinoEEPROMProg/EEPROMProgramer.cs
+++ Windows_Client/ArduinoEEPROMProg/EEPROMProgramer.cs
@@ -42,24 +42,44 @@ namespace ArduinoEEPROMProg
             //clear
             Console.Clear();
 
+            //welcome text
+
+            Console.WriteLine(AsciiHolder.banner);
+
+            Console.WriteLine("Current functions are: \n\tread\n\twrite");
+
             while (true)
             {
                 string entry = Console.ReadLine();
                 if (entry == "read")
                 {
-                    byte[] code = new byte[] { 0x02, 0x00, 0x00, 0x06, 0x00, 0xFF, 0x04, 0x00, 0x00 };
+                    Console.WriteLine("Please enter start address");
+                    uint startAddress = Convert.ToByte(Console.ReadLine(), 16);
+
+                    Console.WriteLine("Please enter end address");
+                    uint endAddress = Convert.ToByte(Console.ReadLine(), 16);
+
+                    byte[] code = new byte[] { 0x02, (byte)(startAddress >> 8), (byte)startAddress, 0x06, (byte)(startAddress >> 8), (byte)endAddress, 0x04, 0x00, 0x00 };
                     con.Write(code);
                     continue;
                 }
 
                 else if (entry == "write")
                 {
+                    Console.WriteLine("Please enter start address");
+                    uint startAddress = Convert.ToByte(Console.ReadLine(), 16);
+
+                    Console.WriteLine("Please enter end address");
+                    uint endAddress = Convert.ToByte(Console.ReadLine(), 16);
+
                     //get value
+                    Console.WriteLine("Please enter value");
                     byte val = Convert.ToByte(Console.ReadLine(), 16);
+
                     byte[] code = new byte[] { 0x01, 0x00, 0x00 };
                     //send
                     con.Write(code);
-                    for (ushort i = 0; i <= 0x00FF; i++)
+                    for (uint i = startAddress; i < endAddress; i++)
                     {
                         byte addH = (byte)(i >> 8);
                         byte addL = (byte)i;
@@ -69,6 +89,10 @@ namespace ArduinoEEPROMProg
                     //end
                     con.Write(new byte[] { 0xFF, 0x00, 0x00 });
                     continue;
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command");
                 }
 
                 try
